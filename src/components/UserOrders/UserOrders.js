451a31cb412dfdda4e15b21c112ab3b8e97dc765
tsukikomiata <85107866+tsukikomiata@users.noreply.github.com>
2023-06-23 17:20:05 +1000

    minor changes

--- src/components/UserOrders/UserOrders.js
+++ src/components/UserOrders/UserOrders.js
@@ -7,9 +7,16 @@ function UserOrders({orders}) {
     let userOrders = []
     orders.map((order) => userOrders.push(order))
     userOrders.reverse()
+    let empty = ""
+    if (!userOrders.length) {
+        empty = "Пусто :("
+    }
+    console.log(userOrders)
     return (
         <div className={styles.userOrders}>
+            <p className={styles.userOrdersTitle}>Заказы: </p>
             {userOrders.map((option) => <OrderCard order={option}/>)}
+            <p>{empty}</p>
             <img src={catLogo} alt="cat" className={styles.userOrders__catLogo}/>
         </div>
     )
